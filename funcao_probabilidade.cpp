070055bc8fd32930b13bcef05951a9ae316c9108
rubens-lavor <haryson07@gmail.com>
2020-10-07 15:32:36 -0300

    update

--- funcao_probabilidade.cpp
+++ funcao_probabilidade.cpp
@@ -2,6 +2,22 @@
 
 #include <iostream>
 
+//Nesse exemplo uma moeda está sendo lançada aleatóriamente 10 vezes.
+//X é o numero de caras. 
+
+/* Calcular:
+    p(x = 1)
+    p(x = 2)
+    p(x = 3)
+    p(x = 4)
+    p(x = 5)
+    p(x = 6)
+    p(x = 7)
+    p(x = 8)
+    p(x = 9)
+    p(x = 10)
+*/
+
 //Função que calcula, recursivamente, o fatorial de um numero.
 unsigned long int fat(int n) {
     if ((n == 1) || (n == 0))
@@ -18,12 +34,12 @@ float func_probabilidade(int x, int n, float p) {
 int main() {
     float prob_x;   // probabilidade de ocorrer x
     float p = 0.5;  // probabilidade de ocorrer sucesso. No meu problema é 50%
-    int x = 1;      // variável aleatória binomial. numero de "sucessos" em n tentativas
+    int x;          // variável aleatória binomial. numero de "sucessos" em n tentativas
     int n = 10;     // numero de ensaios aleatórios independentes reproduzidos
 
-    for (int i = 0; i < 11; i++) {
+    for (int x = 0; x < 11; x++) {
         /* code */
-        prob_x = func_probabilidade(i, n, p);
-        std::cout << "p(x = " << i << " ) = " << prob_x << std::endl;
+        prob_x = func_probabilidade(x, n, p);
+        std::cout << "p(x = " << x << " ) = " << prob_x << std::endl;
     }
 }
